44ec01480ff008f935d0e7189858b043c80d39cd
doram <doramtc8@gmail.com>
2023-10-21 19:01:33 +0200

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using Projekat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string address = "net.tcp://localhost:4000/IService1";
+                NetTcpBinding binding = new NetTcpBinding();
+
+                ChannelFactory<IService1> channel = new ChannelFactory<IService1>(binding, address);
+
+                IService1 proxy = channel.CreateChannel();
+
+                Console.WriteLine("Klijent uspesno pokrenut.");
+
+                Console.WriteLine("Unesite tekst:");
+
+                string unos = Console.ReadLine();
+                Console.WriteLine(proxy.GetData(unos));
+
+                Console.ReadKey();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,29 @@
+﻿using Projekat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(Service1)))
+            {
+                string address = "net.tcp://localhost:4000/IService1";
+                NetTcpBinding binding = new NetTcpBinding();
+
+                host.AddServiceEndpoint(typeof(IService1), binding, address);
+
+                host.Open();
+                Console.WriteLine($"Servis je uspesno pokrenut na adresi : {address}");
+                Console.ReadKey();
+                host.Close();
+            }
+        }
+    }
+}
